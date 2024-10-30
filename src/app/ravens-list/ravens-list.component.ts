0a0b4e79a1fed02ce09e10eaa3d361a4bb3d3dde
isaiah Woodley <W0862252@myscc.ca>
2024-10-30 13:45:09 -0400

    assignment3 forth commit

--- src/app/ravens-list/ravens-list.component.ts
+++ src/app/ravens-list/ravens-list.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {NgForOf, NgIf} from '@angular/common';
+import {RavensListItemComponent} from '../ravens-list-item/ravens-list-item.component';
 
 @Component({
   selector: 'app-ravens-list',
   standalone: true,
   imports: [
     NgForOf,
-    NgIf
+    NgIf,
+    RavensListItemComponent
   ],
   templateUrl: './ravens-list.component.html',
   styleUrl: './ravens-list.component.css'
@@ -18,4 +20,5 @@ export class RavensListComponent {
       {City: 'Baltimore', Team: 'Ravens', HeadCoach: 'John Harbaugh'},
       {City: 'Cleveland', Team: 'Browns', HeadCoach: 'Kevin Stefanski'}
     ];
+  Quarterback: any;
 }
