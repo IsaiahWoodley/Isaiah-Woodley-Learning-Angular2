092727144ccec5329eb40767d9e3fd56a0713183
isaiah Woodley <W0862252@myscc.ca>
2024-11-01 18:03:51 -0400

    assignment6 Second commit

---
+++ src/app/modify/modify.component.ts
@@ -0,0 +1,39 @@
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {NgIf} from '@angular/common';
+
+
+
+@Component({
+  selector: 'app-modify',
+  standalone: true,
+  imports: [
+    FormsModule,
+    NgIf,
+    ReactiveFormsModule
+  ],
+  templateUrl: './modify.component.html',
+  styleUrl: './modify.component.css'
+})
+export class ModifyComponent implements OnInit {
+  modifyForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.modifyForm = this.formBuilder.group({
+      teamName: ['', Validators.required],
+      headCoach: ['', Validators.required],
+      city: ['', Validators.required]
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+
+  onSubmit() {
+    if (this.modifyForm.valid) {
+      console.log(this.modifyForm.value);
+    }
+  }
+
+}
